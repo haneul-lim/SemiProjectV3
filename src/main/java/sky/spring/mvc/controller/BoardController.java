cdd506a2d0b0dad24007ba1380499194a0401f22
haneul-lim <limsky369@gamil.com>
2021-06-09 16:34:23 +0900

    게시판 페이징 기능 추가

--- src/main/java/sky/spring/mvc/controller/BoardController.java
+++ src/main/java/sky/spring/mvc/controller/BoardController.java
@@ -1,14 +1,23 @@
 package sky.spring.mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import sky.spring.mvc.service.BoardService;
 
 @Controller
 public class BoardController {
 
+    @Autowired private BoardService bsrv;
+
     @GetMapping("/board/list")
-    public String list() {
-        return "board/list.tiles";
+    public ModelAndView list(ModelAndView mv, String cp) {
+        if (cp == null) cp = "1";
+        mv.setViewName("board/list.tiles");
+        mv.addObject("bds", bsrv.readBoard(cp));
+
+        return mv;
     }
 
     @GetMapping("/board/view")
--- src/main/webapp/WEB-INF/tiles/template.jsp
+++ src/main/webapp/WEB-INF/tiles/template.jsp
@@ -34,7 +34,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                 <h3>로그인</h3>
-                <button type="button" class="btn btn-light">
+                <button type="button" id="lgmbtn" class="btn btn-light">
                     닫 기</button>
                 </div>
 
@@ -70,7 +70,7 @@
                 </div>
 
                 <div class="modal-footer justify-content-center">
-                    <button type="button" class="btn btn-danger">로그인</button>
+                    <button type="button" id="loginbtn" class="btn btn-danger">로그인</button>
                     <button type="button" class="btn btn-warning">아이디/비밀번호 찾기</button>
                 </div>
             </div>
